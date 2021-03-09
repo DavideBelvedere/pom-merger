e3481ec905f40e6d858e643d88de2fe20956b237
davide.belvedere <davide.belvedere@lynxspa.com>
2021-03-09 16:06:32 +0100

    added requirements.txt

--- configuration.py
+++ configuration.py
@@ -1,5 +1,6 @@
-# project_path = '''/home/davide/Documents/Exabeat/aggregator/exabeat'''
-project_path = '''/home/davide/Documents/Exabeat/aggregator/IPEBDCO'''
+#project_path = '''/home/davide/Documents/Exabeat/aggregator/exabeat'''
+project_path = '''/home/davide/Documents/Exabeat/aggregator/IPEBDAST'''
+#branch_to_merge = '''origin/feature/bpm-dismission-v2'''
 branch_to_merge = '''origin/develop_newpom'''
 #branch_to_merge = '''origin/feature/upgrade-to-cdh-5.16-IEB_1.70.X'''
 issue_pattern = r'''IEB[_-](\d+)'''
